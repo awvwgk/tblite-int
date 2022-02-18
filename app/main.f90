c6e9eb1d48953eb7e077a1c478b7b34a5d79c075
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-02-18 10:37:10 +0100

    Allow adjustment of coordination number

--- app/main.f90
+++ app/main.f90
@@ -14,22 +14,22 @@
 ! limitations under the License.
 
 program integrator
-   use, intrinsic :: iso_fortran_env, only : output_unit
-   use mctc_env, only : wp, error_type, fatal_error, get_argument
-   use mctc_io, only : structure_type, new
-   use mctc_io_symbols, only : symbol_length
-   use mctc_io_convert, only : aatoau
-   use stdlib_io_npy, only : save_npy
-   use tblite_adjlist, only : adjacency_list, new_adjacency_list
-   use tblite_basis_type, only : get_cutoff
-   use tblite_cutoff, only : get_lattice_points
-   use tblite_integral_type, only : integral_type, new_integral
-   use tblite_param, only : param_record
-   use tblite_xtb_calculator, only : xtb_calculator, new_xtb_calculator
-   use tblite_xtb_h0, only : get_selfenergy, get_hamiltonian
-   use tblite_xtb_gfn2, only : new_gfn2_calculator
-   use tblite_xtb_gfn1, only : new_gfn1_calculator
-   use tblite_xtb_ipea1, only : new_ipea1_calculator
+   use, intrinsic :: iso_fortran_env, only: output_unit
+   use mctc_env, only: wp, error_type, fatal_error, get_argument
+   use mctc_io, only: structure_type, new
+   use mctc_io_symbols, only: symbol_length
+   use mctc_io_convert, only: aatoau
+   use stdlib_io_npy, only: save_npy
+   use tblite_adjlist, only: adjacency_list, new_adjacency_list
+   use tblite_basis_type, only: get_cutoff
+   use tblite_cutoff, only: get_lattice_points
+   use tblite_integral_type, only: integral_type, new_integral
+   use tblite_param, only: param_record
+   use tblite_xtb_calculator, only: xtb_calculator, new_xtb_calculator
+   use tblite_xtb_h0, only: get_selfenergy, get_hamiltonian
+   use tblite_xtb_gfn2, only: new_gfn2_calculator
+   use tblite_xtb_gfn1, only: new_gfn1_calculator
+   use tblite_xtb_ipea1, only: new_ipea1_calculator
    implicit none
    character(len=*), parameter :: prog_name = "tblite-int"
 
@@ -42,6 +42,7 @@ program integrator
       logical :: hamiltonian = .false.
       real(wp) :: accuracy = 1.0_wp
       real(wp) :: conv = aatoau
+      real(wp), allocatable :: cn(:)
    end type int_config
 
    type(int_config) :: config
@@ -64,7 +65,7 @@ contains
       type(xtb_calculator) :: calc
       character(len=:), allocatable :: method
       real(wp) :: cutoff
-      real(wp), allocatable :: cn(:), selfenergy(:), lattr(:, :)
+      real(wp), allocatable :: selfenergy(:), lattr(:, :)
       type(integral_type) :: ints
       type(adjacency_list) :: list
       character(len=symbol_length) :: sym(2)
@@ -82,26 +83,21 @@ contains
       else
          method = "gfn2"
          if (allocated(config%method)) method = config%method
-         select case(method)
+         select case (method)
          case default
             call fatal_error(error, "Unknown method '"//method//"' requested")
-         case("gfn2")
+         case ("gfn2")
             call new_gfn2_calculator(calc, mol)
-         case("gfn1")
+         case ("gfn1")
             call new_gfn1_calculator(calc, mol)
-         case("ipea1")
+         case ("ipea1")
             call new_ipea1_calculator(calc, mol)
          end select
       end if
       if (allocated(error)) return
 
-      if (allocated(calc%ncoord)) then
-         allocate(cn(mol%nat))
-         call calc%ncoord%get_cn(mol, cn)
-      end if
-
-      allocate(selfenergy(calc%bas%nsh))
-      call get_selfenergy(calc%h0, mol%id, calc%bas%ish_at, calc%bas%nsh_id, cn=cn, &
+      allocate (selfenergy(calc%bas%nsh))
+      call get_selfenergy(calc%h0, mol%id, calc%bas%ish_at, calc%bas%nsh_id, cn=config%cn, &
          & selfenergy=selfenergy)
 
       cutoff = get_cutoff(calc%bas, config%accuracy)
@@ -113,11 +109,13 @@ contains
          & ints%overlap, ints%dipole, ints%quadrupole, ints%hamiltonian)
 
       if (config%hamiltonian) then
+         call write_matrix(ints%hamiltonian)
          call save_npy(config%output_file, ints%hamiltonian)
-         write(output_unit, '(a)') "[Info] Hamiltonian written to '"//config%output_file//"'"
+         write (output_unit, '(a)') "[Info] Hamiltonian written to '"//config%output_file//"'"
       else
+         call write_matrix(ints%overlap)
          call save_npy(config%output_file, ints%overlap)
-         write(output_unit, '(a)') "[Info] Overlap written to '"//config%output_file//"'"
+         write (output_unit, '(a)') "[Info] Overlap written to '"//config%output_file//"'"
       end if
 
    end subroutine main
@@ -131,21 +129,21 @@ contains
 
       iarg = 0
       narg = command_argument_count()
-      do while(iarg < narg)
+      do while (iarg < narg)
          iarg = iarg + 1
          call get_argument(iarg, arg)
-         select case(arg)
+         select case (arg)
          case default
-            if (.not.allocated(config%species1)) then
+            if (.not. allocated(config%species1)) then
                call move_alloc(arg, config%species1)
                cycle
             end if
-            if (.not.allocated(config%species2)) then
+            if (.not. allocated(config%species2)) then
                call move_alloc(arg, config%species2)
                cycle
             end if
-            if (.not.allocated(config%distance)) then
-               allocate(config%distance(3))
+            if (.not. allocated(config%distance)) then
+               allocate (config%distance(3))
                call get_argument_as_realv(arg, config%distance, error)
                if (allocated(error)) exit
                cycle
@@ -153,41 +151,55 @@ contains
             call fatal_error(error, "Too many positional arguments present")
             exit
 
-         case("--help")
+         case ("--help")
             call help(output_unit)
             stop
 
-         case("--method")
+         case ("--method")
             if (allocated(config%param)) then
                call fatal_error(error, "Cannot specify method if parameter file is provided")
                exit
             end if
             iarg = iarg + 1
             call get_argument(iarg, config%method)
-            if (.not.allocated(config%method)) then
+            if (.not. allocated(config%method)) then
                call fatal_error(error, "Missing argument for method")
                exit
             end if
 
-         case("--param")
+         case ("--cn")
+            iarg = iarg + 1
+            call get_argument(iarg, arg)
+            if (.not. allocated(arg)) then
+               call fatal_error(error, "Missing argument for coordination number")
+               exit
+            end if
+            allocate (config%cn(2))
+            call get_argument_as_realv(arg, config%cn, error)
+            if (allocated(error)) exit
+
+         case ("--hamiltonian")
+            config%hamiltonian = .true.
+
+         case ("--param")
             if (allocated(config%param)) then
                call fatal_error(error, "Cannot specify parameter file if method is provided")
                exit
             end if
             iarg = iarg + 1
             call get_argument(iarg, config%param)
-            if (.not.allocated(config%param)) then
+            if (.not. allocated(config%param)) then
                call fatal_error(error, "Missing argument for param")
                exit
             end if
 
-         case("--bohr")
+         case ("--bohr")
             config%conv = 1.0_wp
 
-         case("-o", "--output")
+         case ("-o", "--output")
             iarg = iarg + 1
             call get_argument(iarg, arg)
-            if (.not.allocated(arg)) then
+            if (.not. allocated(arg)) then
                call fatal_error(error, "Output file name not specified")
                exit
             end if
@@ -196,11 +208,11 @@ contains
          end select
       end do
 
-      if (.not.allocated(config%output_file)) then
+      if (.not. allocated(config%output_file)) then
          config%output_file = "tblite.npy"
       end if
 
-      if (.not.all([allocated(config%species1), allocated(config%species2), &
+      if (.not. all([allocated(config%species1), allocated(config%species2), &
          & allocated(config%distance)])) then
          call help(output_unit)
          call fatal_error(error, "Error: Missing positional arguments")
@@ -221,11 +233,11 @@ contains
       character(len=*), parameter :: sep = ","
       character(len=:), allocatable :: targ
 
-      if (.not.allocated(arg)) then
+      if (.not. allocated(arg)) then
          call fatal_error(error, "Cannot read real value, argument missing")
          return
       end if
-      allocate(character(len=len(arg)) :: targ)
+      allocate (character(len=len(arg)) :: targ)
       do i = 1, len(arg)
          if (arg(i:i) == sep) then
             targ(i:i) = " "
@@ -233,7 +245,7 @@ contains
             targ(i:i) = arg(i:i)
          end if
       end do
-      read(targ, *, iostat=stat) val
+      read (targ, *, iostat=stat) val
       if (stat /= 0) then
          call fatal_error(error, "Cannot read real value from '"//arg//"'")
          return
@@ -245,42 +257,87 @@ contains
       integer, intent(in) :: io
       character(len=*), parameter :: nl = new_line('a')
       character(len=*), parameter :: help_text = &
-         "Usage: "//prog_name//" [options] <element1> <element2> <distance>"//nl//&
-         ""//nl//&
-         "Takes two element symbols and a distance to calculate the overlap."//nl//&
-         "Distance vector is given as three comma-separated values in Ångström."//nl//&
-         ""//nl//&
-         "Options"//nl//&
-         ""//nl//&
-         "      --method <name>     Parametrization of the xTB Hamiltonian to use"//nl//&
-         "                          Available methods: gfn1, gfn2, ipea1 (Default: gfn2)"//nl//&
-         "      --param <file>      Parametrization file to use for calculation"//nl//&
-         "      --hamiltonian       Calculate Hamiltonian instead of overlap integrals"//nl//&
-         "      --bohr              Use Bohr instead of Angstrom as distance unit"//nl//&
-         "  -o, --output <file>     Output file for writing Hamiltonian"//nl//&
-         "      --help              Show this help message"//nl//&
-         ""
+                                     "Usage: "//prog_name//" [options] <element1> <element2> <distance>"//nl// &
+                                     ""//nl// &
+                                     "Takes two element symbols and a distance to calculate the overlap."//nl// &
+                                     "Distance vector is given as three comma-separated values in Ångström."//nl// &
+                                     ""//nl// &
+                                     "Options"//nl// &
+                                     ""//nl// &
+                                     "      --method <name>     Parametrization of the xTB Hamiltonian to use"//nl// &
+                                     "                          Available methods: gfn1, gfn2, ipea1 (Default: gfn2)"//nl// &
+                                     "      --param <file>      Parametrization file to use for calculation"//nl// &
+                                     "      --hamiltonian       Calculate Hamiltonian instead of overlap integrals"//nl// &
+                                     "      --bohr              Use Bohr instead of Angstrom as distance unit"//nl// &
+                                     "      --cn <real>,<real>  Coordination number of the two atomic species"//nl// &
+                                     "  -o, --output <file>     Output file for writing Hamiltonian"//nl// &
+                                     "      --help              Show this help message"//nl// &
+                                     ""
 
-         write(io, '(a)') help_text
-      end subroutine help
+      write (io, '(a)') help_text
+   end subroutine help
 
    subroutine handle_error(error)
-      use, intrinsic :: iso_fortran_env, only : error_unit
+      use, intrinsic :: iso_fortran_env, only: error_unit
       type(error_type), allocatable, intent(inout) :: error
 
       interface
          subroutine sys_exit(stat) bind(c, name="exit")
-            use, intrinsic :: iso_c_binding, only : c_int
+            use, intrinsic :: iso_c_binding, only: c_int
             integer(c_int), value :: stat
          end subroutine sys_exit
       end interface
 
       if (allocated(error)) then
-         write(error_unit, '(a)') error%message
+         write (error_unit, '(a)') error%message
 
          ! Use exit(3) to get consistent behaviour across compilers
          call sys_exit(1)
       end if
    end subroutine handle_error
+
+   subroutine write_matrix(matrix, name, unit, step)
+      real(wp), intent(in) :: matrix(:, :)
+      character(len=*), intent(in), optional :: name
+      integer, intent(in), optional :: unit
+      integer, intent(in), optional :: step
+      integer :: d1, d2
+      integer :: i, j, k, l, istep, iunit
+
+      d1 = size(matrix, dim=1)
+      d2 = size(matrix, dim=2)
+
+      if (present(unit)) then
+         iunit = unit
+      else
+         iunit = output_unit
+      end if
+
+      if (present(step)) then
+         istep = step
+      else
+         istep = 5
+      end if
+
+      if (present(name)) write (iunit, '("matrix printed:",1x,a)') name
+
+      do i = 1, d2, istep
+         l = min(i + istep - 1, d2)
+         write (iunit, '(/,6x)', advance='no')
+         do k = i, l
+            write (iunit, '(9x,i7)', advance='no') k
+         end do
+         write (iunit, '(a)')
+         do j = 1, d1
+            write (iunit, '(i6)', advance='no') j
+            do k = i, l
+               write (iunit, '(1x,f15.8)', advance='no') matrix(j, k)
+            end do
+            write (iunit, '(a)')
+         end do
+      end do
+      write (iunit, '(a)')
+
+   end subroutine write_matrix
 
 end program integrator
